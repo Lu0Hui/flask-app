934b0231ad9fa138a42a8edeaa87251a4090e0a3
ipyth <mylhmail@163.com>
2018-07-04 00:13:59 +0800

    feature(user): Add user api
    Add feature for manage user.

--- apps/__init__.py
+++ apps/__init__.py
@@ -7,4 +7,7 @@ def create_app():
     from apps.core.views import core as core_blueprint
     app.register_blueprint(core_blueprint, url_prefix='/')
 
+    from apps.users.views import user as user_blueprint
+    app.register_blueprint(user_blueprint, url_prefix='/users')
+
     return app
---
+++ apps/users/views.py
@@ -0,0 +1,42 @@
+from flask import Blueprint
+from flask import jsonify
+from flask_restful import Api, Resource
+
+user = Blueprint('user', __name__)
+api = Api(user)
+
+
+class UserAPI(Resource):
+
+    def get(self, user_id=None):
+        if user_id is None:
+            return jsonify(
+                request="GET /users/",
+                message="Give a list of all users"
+            )
+        else:
+            return jsonify(
+                request="GET /users/<user_id>",
+                message="Show a single user: {}".format(user_id)
+            )
+
+    def post(self):
+        return jsonify(
+            request="POST /users/",
+            message="Create a new user"
+        )
+
+    def put(self, user_id):
+        return jsonify(
+            request="PUT /users/<user_id>",
+            message="Update a single user: {}".format(user_id)
+        )
+
+    def delete(self, user_id):
+        return jsonify(
+            request="DELETE /users/<user_id>",
+            message="Delete a single user: {}".format(user_id)
+        )
+
+
+api.add_resource(UserAPI, '/', '/<user_id>')
